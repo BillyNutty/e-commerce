1d7035fc895daddd94e88bcf413e883abb562de0
Williiam Notturno <williiamnotturno@Williams-iMac.fios-router.home>
2023-12-26 21:54:58 -0500

    responsive

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -4,12 +4,12 @@ import './NewsLetter.css'
 const NewsLetter = () => {
   return (
     <div className='newsletter'>
-    <h1>Get Exclusive Offers On Your E-mail</h1>
-    <p>Subscribe to our newsletter and stay updated</p>
-    <div>
-    <input type="email" placeholder='Your E-mail' />
-    <button>Subscribe</button>
-    </div>
+        <h1>Get Exclusive Offers On Your E-mail</h1>
+        <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type="email" placeholder='Your E-mail' />
+        <button>Subscribe</button>
+     </div>
     </div>
   )
 }
